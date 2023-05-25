10e65317a463dc1533400e5595323802492e3c8e
pingguoshuai <908008766@qq.com>
2023-05-25 17:19:29 +0800

    添加common模块

---
+++ modules/Common/Fast.Template.Common.Utils/SelectItem.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Fast.Template.Common.Utils
+{
+    /// <summary>
+    /// 列表项
+    /// </summary>
+    public class SelectItem
+    {
+        public SelectItem(string key, string value)
+        {
+            Key = key;
+            Value = value;
+        }
+
+        public string Key { get; set; }
+        public string Value { get; set; }
+    }
+
+    //public class SelectItem : SelectItem<string>
+    //{
+    //    public SelectItem(string text, string value) : base(text, value)
+    //    {
+    //    }
+    //}
+
+    //public class SelectItem<T>
+    //{
+    //    public SelectItem(string text, T value)
+    //    {
+    //        Text = text;
+    //        Value = value;
+    //    }
+
+    //    public string Text { get; set; }
+    //    public T Value { get; set; }
+    //}
+}
