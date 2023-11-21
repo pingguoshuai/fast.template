753058602abde5859f97f2a3b795e55eabe8b5b1
Shuai <908008766@qq.com>
2023-11-21 23:03:33 +0800

    自定义路由

---
+++ ui/manage/src/router/model.ts
@@ -0,0 +1,23 @@
+﻿
+interface MenuRoute{
+    path: string;
+    name?: string;
+    component: any;
+    redirect?: string;
+    parentName?: string;
+    requiredPolicy?: string;
+    order?: number;
+    meta?: MenuMeta;
+}
+
+interface MenuMeta{
+    title?: string;
+    isLink?: string;
+    isHide?: boolean;
+    isKeepAlive?: boolean;
+    isAffix?: boolean;
+    isIframe?: boolean;
+    order?: number;
+    icon?: string;
+    roles?: string[];
+}
---
+++ ui/manage/src/router/routes/modules/authCenter.ts
@@ -0,0 +1,90 @@
+﻿import {LAYOUT} from "/@/router/constant";
+
+const authCenterRouter: MenuRoute[] = [
+    {
+        path: '/authCenter',
+        name: 'authCenter',
+        redirect: '/authCenter/apiScope',
+        component: LAYOUT,
+        meta:{
+            title: 'message.router.authCenter',
+            isLink: '',
+            isHide: false,
+            isKeepAlive: true,
+            isAffix: false,
+            isIframe: false,
+            roles: ['admin', 'common'],
+            icon: 'iconfont icon-quanxian'
+        }
+    },
+    {
+        path: '/authCenter/apiScope',
+        name: 'apiScope',
+        component: () => import('/@/views/authcenter/apiscope/index.vue'),
+        meta: {
+            title: 'message.router.authApiScope',
+            isLink: '',
+            isHide: false,
+            isKeepAlive: true,
+            isAffix: false,
+            isIframe: false,
+            roles: ['admin', 'common'],
+            icon: 'iconfont icon-xitongshezhi',
+        },
+        parentName: 'authCenter',
+        requiredPolicy:'IdsAdmin.ApiScope',
+    },
+    {
+        path: '/authCenter/identityResource',
+        name: 'identityResource',
+        component: () => import('/@/views/authcenter/identityresource/index.vue'),
+        meta: {
+            title: 'message.router.authIdentityResource',
+            isLink: '',
+            isHide: false,
+            isKeepAlive: true,
+            isAffix: false,
+            isIframe: false,
+            roles: ['admin', 'common'],
+            icon: 'iconfont icon-xitongshezhi',
+        },
+        parentName: 'authCenter',
+        requiredPolicy:'IdsAdmin.IdentityResource',
+    },
+    {
+        path: '/authCenter/apiResource',
+        name: 'apiResource',
+        component: () => import('/@/views/authcenter/apiresource/index.vue'),
+        meta: {
+            title: 'message.router.authApiResource',
+            isLink: '',
+            isHide: false,
+            isKeepAlive: true,
+            isAffix: false,
+            isIframe: false,
+            roles: ['admin', 'common'],
+            icon: 'iconfont icon-xitongshezhi',
+        },
+        parentName: 'authCenter',
+        requiredPolicy:'IdsAdmin.ApiResource',
+    },
+    {
+        path: '/authCenter/apiClient',
+        name: 'apiClient',
+        component: () => import('/@/views/authcenter/apiclient/index.vue'),
+        meta: {
+            title: 'message.router.authApiClient',
+            isLink: '',
+            isHide: false,
+            isKeepAlive: true,
+            isAffix: false,
+            isIframe: false,
+            roles: ['admin', 'common'],
+            icon: 'iconfont icon-xitongshezhi',
+        },
+        parentName: 'authCenter',
+        requiredPolicy:'IdsAdmin.Client',
+    },
+];
+
+export default authCenterRouter;
